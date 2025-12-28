de434b9ad18810f345f4b55112f8a721198a7e02
Mateusz Zieliński <matzielinski7@gmail.com>
2025-12-28 11:33:27 +0100

    SimulationStats header fix

---
+++ DarwinWorld/src/main/java/agh/model/util/SimulationStats.java
@@ -0,0 +1,6 @@
+package agh.model.util;
+
+public class SimulationStats {
+//    int livingAnimalsCount, int grassesCount, int freeFields, Genotype mostPopularGenotype,
+//    int avgEnergyLevel, int avgLifeTime, int avgChildCount
+}

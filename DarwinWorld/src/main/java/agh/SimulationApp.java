bfd0c3456d91267ce24ea58c81109c6a2e0abd52
JohnyXDDD <jan.lenart.jl@gmail.com>
2025-12-28 20:10:14 +0100

    Configuration screen

--- DarwinWorld/src/main/java/agh/SimulationApp.java
+++ DarwinWorld/src/main/java/agh/SimulationApp.java
@@ -6,31 +6,35 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class SimulationApp extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-//        FXMLLoader loader = new FXMLLoader();
-//        loader.setLocation(getClass().getClassLoader().getResource("simulation.fxml"));
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getClassLoader().getResource("configurationScreen.fxml"));
 //
-//        BorderPane viewRoot = loader.load();
-//        SimulationPresenter presenter = loader.getController();
-//        configureStage(primaryStage, viewRoot);
-//        primaryStage.show();
-//    }
+        StackPane viewRoot = loader.load();
+        SimulationPresenter presenter = loader.getController();
+        configureStage(primaryStage, viewRoot);
+        primaryStage.show();
+    }
 //
-//    private void configureStage(Stage primaryStage, BorderPane viewRoot) {
-//        // stworzenie sceny (panelu do wyświetlania wraz zawartością z FXML)
-//        var scene = new Scene(viewRoot);
-//
-//        // ustawienie sceny w oknie
-//        primaryStage.setScene(scene);
-//
-//        // konfiguracja okna
-//        primaryStage.setTitle("Simulation app");
+    private void configureStage(Stage primaryStage, StackPane viewRoot) {
+        // Do posprzątania
+        Font firaFont = Font.loadFont(getClass().getResourceAsStream("/fonts/FiraCode-Regular.ttf"), 12);
+        Font firaFont2 = Font.loadFont(getClass().getResourceAsStream("/fonts/FiraCode-Bold.ttf"), 12);
+        var scene = new Scene(viewRoot);
+        scene.getStylesheets().add(getClass().getResource("/style.css").toExternalForm());
+
+        // ustawienie sceny w oknie
+        primaryStage.setScene(scene);
+
+        // konfiguracja okna
+        primaryStage.setTitle("Simulation app");
 //        primaryStage.minWidthProperty().bind(viewRoot.minWidthProperty());
 //        primaryStage.minHeightProperty().bind(viewRoot.minHeightProperty());
-//    }
-}
+    }
 }
